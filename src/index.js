ba37052123658150baf202a81caa3fa6e58b6c32
Wolfgang <wolf@okwolf.com>
2017-11-25 15:17:06 -0800

    move esm config to package.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Move this after the @std/esm require and coverage for cjs goes away also
 require("./cjs/main")();
 
-require = require("@std/esm")(module, { esm: "js", cjs: true });
+require = require("@std/esm")(module);
 // No coverage for code called below here
 require("./esm/main").default();
